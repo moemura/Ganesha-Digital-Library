9106f9aef69f70879499b597c6c9bdf1349cacef
moemura <srakti@live.com>
2019-07-24 17:34:58 +0700

    chage encoding to utf8

--- src/gdl42/schema/upload/dc_document_values.php
+++ src/gdl42/schema/upload/dc_document_values.php
@@ -13,7 +13,7 @@ if (!isset($frm['LANGUAGE'])) {
 }
 if (!isset($frm['RELATION_COUNT'])) $frm['RELATION_COUNT']=0;
 if (!isset($frm['DATE'])) $frm['DATE']=date("Y-m-d");
-$frm['RIGHTS'] = "Copyright © 2001 by $gdl_publisher[publisher]. Verbatim copying and distribution of this entire article is permitted by author in any medium, provided this notice is preserved.";
+$frm['RIGHTS'] = "Copyright Â© 2001 by $gdl_publisher[publisher]. Verbatim copying and distribution of this entire article is permitted by author in any medium, provided this notice is preserved.";
 $frm['IDENTIFIER_HIERARCHY']=$gdl_folder->get_hierarchy($_SESSION['gdl_node']);
 
 ?>
