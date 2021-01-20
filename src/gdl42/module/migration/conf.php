b9617061e77037c6a7e6b1123771c502448d70b6
moemura <srakti@live.com>
2021-01-20 17:50:45 +0700

    Delete conf.php

--- src/gdl42/module/migration/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_source["host"]="localhost";
-$db_source["uname"]="root";
-$db_source["password"]="idlnhub";
-$db_source["name"]="gdlPP";
-
-$gdl_modul['name'] = _MIGRATION4042;
-$gdl_menu['configuration'] = _CONFIGURATION;
-?>
